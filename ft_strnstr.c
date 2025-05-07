b65546be448dc22c303c634bca196708b225c90d
moabe <moabe@student.42tokyo.jp>
2025-05-07 21:00:22 +0900

    debug strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,30 +14,27 @@
 
 char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
-	int		i;
-	int		j;
-	char	*p;
+	unsigned int		i;
+	unsigned int		j;
 
+	if (!*to_find)
+		return ((char *)str);
 	i = 0;
-	j = 0;
-	p = (char *)str;
-	while (p[i] != '\0' && i < (int)len)
+	while (str[i] && (size_t)i < len)
 	{
-		if (*to_find == '\0')
-			return (p);
-		if (p[i] == to_find[0])
+		if (str[i] == to_find[0])
 		{
-			j = 0;
-			while (p[i + j] != '\0' && to_find[j] != '\0'
-				&& p[i + j] == to_find[j])
+			j = 1;
+			while (to_find[j] && str[i + j] == to_find[j] && (size_t)(i + j) < len)
 				j++;
 			if (to_find[j] == '\0')
-				return (&p[i]);
+				return ((char *)&str[i]);
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
 
 // int main(void)
 // {
